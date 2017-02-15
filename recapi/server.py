88b072d22cd5601769587ac3202e304653eed4de
Christoph Schock <chschock@gmail.com>
2017-02-15 18:55:00 +0100

    v1

--- recapi/server.py
+++ recapi/server.py
@@ -1,99 +1,95 @@
 import os
+from flask import Flask
+from flask_restful import Api, abort
+from flask_sqlalchemy import SQLAlchemy
+
+from recapi.config import DevelopmentConfig
+
+from recapi.database import init_db
+
+app = Flask(__name__)
+api = Api(app)
+app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
 from flask import jsonify
 from flask_restful import Resource, reqparse
+from recapi.cache import Cache, Like
 
-from recapi import app, api, db
-from recapi.cache import Cache
-
-cache = Cache(db)
-
+cache = None
 users = dict()
 
-class UserListAPI(Resource):
+
+class LikesAPI(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('page', type=int, required=False, location='json')
-        self.reqparse.add_argument('n_results', type=int, required=False, location='json')
-        super(UserListAPI, self).__init__()
+        # self.reqparse.add_argument('user_id', type=str, required=True, location='json')
+        # self.reqparse.add_argument('like_id', type=str, location='json')
+        super(LikesAPI, self).__init__()
 
-    def get(self):
-        return jsonify(users.keys())
-
-api.add_resource(UserListAPI, '/v1/users', endpoint='users')
-
-class UserAPI(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('user_id', type=str, location='json')
-        super(UserAPI, self).__init__()
-
-    def post(self, user_id):
+    def post(self, user_id, item_id):
         args = self.reqparse.parse_args()
-        if user_id in users:
-            abort(409)
-        users[user_id] = set()
-        return jsonify( { 'user': user_id } )
+        cache.add(Like(user_id, item_id))
 
-    def delete(self, user_id):
+    def delete(self, user_id, item_id):
         args = self.reqparse.parse_args()
-        if user_id not in users:
-            abort(404)
-        del users[user_id]
-        return jsonify( { 'user': user_id } )
+        cache.delete(Like(user_id, item_id))
 
-api.add_resource(UserAPI, '/v1/users/<string:user_id>', endpoint='user')
+# api.add_resource(UserAPI, '/v1/users/<string:user_id>', endpoint='user')
+api.add_resource(LikesAPI, '/v1/likes/user/<string:user_id>/item/<string:item_id>', endpoint='userlike')
 
-class UserLikeListAPI(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('page', type=int, required=False, location='json')
-        self.reqparse.add_argument('n_results', type=int, required=False, location='json')
-        super(UserListAPI, self).__init__()
+cache = None
 
-    def get(self):
-        args = self.reqparse.parse_args()
-        if user_id not in users:
-            abort(404)
-        return jsonify(list(users[user_id]))
-
-api.add_resource(UserLikeListAPI, '/v1/users/<string:user_id>/likes', endpoint='userlikes')
-
-class UserLikeAPI(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('user_id', type=str, required=True, location='json')
-        self.reqparse.add_argument('like_id', type=str, location='json')
-        super(UserLikeAPI, self).__init__()
-
-    def post(self, user_id, like_id):
-        args = self.reqparse.parse_args()
-        if user_id not in users:
-            abort(404)
-        if like_id not in users[user_id]:
-            user[user_id].add(like_id)
-
-    def delete(self, user_id, like_id):
-        args = self.reqparse.parse_args()
-        if user_id not in users:
-            abort(404)
-        if like_id in users[user_id]:
-            user[user_id].remove(like_id)
-
-
-api.add_resource(UserLikeAPI, '/v1/users/<string:user_id>/likes/<string:like_id>', endpoint='userlike')
-
-
-# class ItemAPI(Resource):
-#     def get(self, id):
-#         pass
-#
-#     def put(self, id):
-#         pass
-#
-#     def delete(self, id):
-#         pass
-#
-# api.add_resource(UserAPI, '/v1/users/<int:id>', endpoint='user')
+def init_api():
+    global cache
+    with app.app_context():
+        cache = Cache(db)
 
 if __name__ == '__main__':
+    init_api()
     app.run(debug = True)
+
+
+# class UserListAPI(Resource):
+#     def __init__(self):
+#         self.reqparse = reqparse.RequestParser()
+#         self.reqparse.add_argument('page', type=int, required=False, location='json')
+#         self.reqparse.add_argument('n_results', type=int, required=False, location='json')
+#         super(UserListAPI, self).__init__()
+#
+#     def get(self):
+#         return jsonify(users.keys())
+#
+# api.add_resource(UserListAPI, '/v1/users', endpoint='users')
+
+# class UserAPI(Resource):
+#     def __init__(self):
+#         self.reqparse = reqparse.RequestParser()
+#         self.reqparse.add_argument('user_id', type=str, location='json')
+#         super(UserAPI, self).__init__()
+#
+#     def get(self, user_id):
+#         return jsonify(len(cache))
+#         # return jsonify('hello')
+#
+#     def delete(self, user_id):
+#         args = self.reqparse.parse_args()
+#         if user_id not in users:
+#             abort(404)
+#         del users[user_id]
+#         return jsonify( { 'user': user_id } )
+
+# class UserLikeListAPI(Resource):
+#     def __init__(self):
+#         self.reqparse = reqparse.RequestParser()
+#         self.reqparse.add_argument('page', type=int, required=False, location='json')
+#         self.reqparse.add_argument('n_results', type=int, required=False, location='json')
+#         super(UserListAPI, self).__init__()
+#
+#     def get(self):
+#         args = self.reqparse.parse_args()
+#         if user_id not in users:
+#             abort(404)
+#         return jsonify(list(users[user_id]))
+#
+# api.add_resource(UserLikeListAPI, '/v1/users/<string:user_id>/likes', endpoint='userlikes')
--- tests/test_users.py
+++ tests/test_users.py
@@ -3,48 +3,50 @@ import unittest
 import string
 import random
 
-# from psycopg2 import IntegrityError
-# import psycopg2
 from sqlalchemy.exc import IntegrityError
 
-from recapi import app, db
+from recapi.server import app, db, init_api
 from recapi.config import basedir
-from recapi.models import Like
-from recapi.cache import Cache
+from recapi.models import DbLike
+from recapi.cache import Cache, Like
+
 
 LC = string.ascii_lowercase
 UC = string.ascii_uppercase
 USER_IDS = [a + b for a in UC for b in LC]
 ITEM_IDS = [a + b for a in LC for b in LC]
-LIKES = [(user, item) for user in USER_IDS for item in ITEM_IDS]
+LIKES = [Like(user, item) for user in USER_IDS for item in ITEM_IDS]
 
 def random_like():
-    return random.choice(USER_IDS[:5]), random.choice(ITEM_IDS[:5])
+    return Like(random.choice(USER_IDS[:5]), random.choice(ITEM_IDS[:5]))
 
 def random_likes(count):
     return random.sample(LIKES, count)
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        app.config['TESTING'] = True
+        # app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///api'
-        self.app = app.test_client()
-        db.drop_all()
-        db.create_all()
+        self.client = app.test_client()
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
+        init_api()
 
     def tearDown(self):
         db.session.remove()
-        # db.drop_all()
+        # with app.app_context():
+        #     db.drop_all()
 
     def test_db_insert_unique(self):
-        for user_id, item_id in random_likes(1000):
-            like = Like(user_id, item_id)
+        unique_likes = random_likes(1000)
+        for user_id, item_id in unique_likes:
+            like = DbLike(user_id, item_id)
             db.session.add(like)
         db.session.commit()
-        res = [like.tuple for like in Like.query.all()]
-        print('%d likes in db.' % len(res))
-        print(list(res)[:4])
+        res = [like.tuple for like in DbLike.query.all()]
+        self.assertCountEqual(unique_likes, res)
 
     def test_cache_add_many(self):
         unique_likes = random_likes(1000)
@@ -52,7 +54,7 @@ class TestCase(unittest.TestCase):
         cache = Cache(db)
         cache.add_many(unique_likes)
         cache.add_many(extra_likes)
-        self.assertEqual(len(cache), len(unique_likes))
+        self.assertCountEqual(cache.likes, unique_likes)
 
     def test_cache_add(self):
         unique_likes = random_likes(20)
@@ -60,18 +62,37 @@ class TestCase(unittest.TestCase):
         cache = Cache(db)
         for like in unique_likes + extra_likes:
             cache.add(like)
-        self.assertEqual(len(cache), len(unique_likes))
+        self.assertCountEqual(cache.likes, unique_likes)
 
     def test_cache_persistence(self):
         unique_likes = random_likes(1000)
         Cache(db).add_many(unique_likes)
         cache = Cache(db)
-        self.assertEqual(len(cache), len(unique_likes))
+        self.assertCountEqual(cache.likes, unique_likes)
 
+    # def test_api(self):
+    #     result = self.client.get('/v1/users/asdf')
+    #     print('result: %s' % result.data.decode())
+
+    def test_user_like_api_post(self):
+        unique_likes = random_likes(20)
+        extra_likes = unique_likes[:10]
+        for user_id, item_id in unique_likes + extra_likes:
+            result = self.client.post('/v1/likes/user/' + user_id + '/item/' + item_id, data={})
+        self.assertEqual(len(DbLike.query.all()), len(unique_likes))
+
+    def test_user_like_api_delete(self):
+        unique_likes = random_likes(20)
+        extra_likes = unique_likes[:10]
+        for user_id, item_id in unique_likes:
+            result = self.client.post('/v1/likes/user/' + user_id + '/item/' + item_id, data={})
+        for user_id, item_id in extra_likes:
+            result = self.client.delete('/v1/likes/user/' + user_id + '/item/' + item_id, data={})
+        self.assertEqual(len(DbLike.query.all()), len(unique_likes) - len(extra_likes))
 
     # def test_db_insert(self):
     #     for i in range(100000):
-    #         like = Like(*random_like())
+    #         like = DbLike(*random_like())
     #         db.session.add(like)
     #     try:
     #         db.session.commit()
