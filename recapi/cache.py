ed50a5036efdefb8c4613dd4cd216a45ec8e04e6
Christoph Schock <chschock@gmail.com>
2017-02-16 02:30:18 +0100

    adapt cache to coo_matrix constructor

--- recapi/cache.py
+++ recapi/cache.py
@@ -14,30 +14,43 @@ from .models import DbLike
     # stmt = insert('likes').values([{'user_id': 'qwe', 'item_id': 'bnm'}])
     # self.db.engine.execute(stmt)
 
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 Like = namedtuple('Like', 'user_id item_id')
 
 class Cache(object):
     def __init__(self, db):
-        self.likes = set([like.tuple for like in DbLike.query.all()])
         self.db = db
+        self.user_map = defaultdict(lambda: len(self.user_map))
+        self.item_map = defaultdict(lambda: len(self.item_map))
+
+        self.likes = set([Like(*like.tuple) for like in DbLike.query.all()])
+        self.user_ics = [self.user_map[like.user_id] for like in self.likes]
+        self.item_ics = [self.item_map[like.item_id] for like in self.likes]
+        self.ratings = [1] * len(self.likes)
 
     def add_many(self, likes):
         for like in likes:
             assert isinstance(like, Like)
             if like not in self.likes:
                 self.db.session.add(DbLike(*like))
-
         self.db.session.commit()
-        self.likes.update(likes)
+
+        new_likes = [like for like in likes if like not in self.likes]
+        self.likes.update(new_likes)
+        self.user_ics.extend([self.user_map[like.user_id] for like in new_likes])
+        self.item_ics.extend([self.item_map[like.item_id] for like in new_likes])
+        self.ratings.extend([1] * len(new_likes))
 
     def add(self, like):
         assert isinstance(like, Like)
-        if like in self.likes:
-            return
-        self.db.session.add(DbLike(*like))
-        self.db.session.commit()
-        self.likes.add(like)
+        if like not in self.likes:
+            self.db.session.add(DbLike(*like))
+            self.db.session.commit()
+
+            self.likes.add(like)
+            self.user_ics.append(self.user_map[like.user_id])
+            self.item_ics.append(self.item_map[like.item_id])
+            self.ratings.append(1)
 
     def delete(self, like):
         assert isinstance(like, Like)
@@ -45,8 +58,11 @@ class Cache(object):
             DbLike.query.filter_by(
                 user_id=like.user_id, item_id=like.item_id).delete()
             self.db.session.commit()
-            self.likes.discard(like)
 
+            self.likes.discard(like)
+            self.user_ics.append(self.user_map[like.user_id])
+            self.item_ics.append(self.item_map[like.item_id])
+            self.ratings.append(-1)
 
     def __len__(self):
         return len(self.likes)
