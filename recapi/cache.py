a4f1004e26902f2d84916509d38fe73c7a7c9c45
Christoph Schock <chschock@gmail.com>
2017-02-18 19:45:56 +0100

    cache recommender with changes threshold

--- recapi/cache.py
+++ recapi/cache.py
@@ -5,8 +5,16 @@ from .recommender import Recommender
 import time
 Like = namedtuple('Like', 'user_id item_id')
 
-class Cache(object):
+REBUILD_RECOMMENDER_CHANGE_THRESHOLD = 100
 
+class Cache(object):
+    """
+    Cache serves as in memory structure mirroring all data that is also
+    persisted in the database. In the cache, data is stored in a normalized
+    form, ready to be converted to a Recommender object, which is also stored
+    in the cache. When the recommender is out of date in terms of the number
+    of changes made to the data model, it is rebuilt on request.
+    """
     def __init__(self, db):
         self.db = db
         self.user_map = defaultdict(lambda: len(self.user_map))
@@ -16,6 +24,9 @@ class Cache(object):
         self.user_ics = [self.user_map[like.user_id] for like in self.likes]
         self.item_ics = [self.item_map[like.item_id] for like in self.likes]
         self.ratings = [1] * len(self.likes)
+
+        self.recommender = None
+        self.changes = 0
 
     def add_many(self, likes):
         for like in likes:
@@ -29,6 +40,7 @@ class Cache(object):
         self.user_ics.extend([self.user_map[like.user_id] for like in new_likes])
         self.item_ics.extend([self.item_map[like.item_id] for like in new_likes])
         self.ratings.extend([1] * len(new_likes))
+        self.changes += len(new_likes)
 
     def add(self, like):
         assert isinstance(like, Like)
@@ -40,6 +52,7 @@ class Cache(object):
             self.user_ics.append(self.user_map[like.user_id])
             self.item_ics.append(self.item_map[like.item_id])
             self.ratings.append(1)
+            self.changes += 1
 
     def delete(self, like):
         assert isinstance(like, Like)
@@ -52,11 +65,19 @@ class Cache(object):
             self.user_ics.append(self.user_map[like.user_id])
             self.item_ics.append(self.item_map[like.item_id])
             self.ratings.append(-1)
+            self.changes += 1
 
     def __len__(self):
         return len(self.likes)
 
+    def out_of_date(self):
+        return (self.recommender is None or
+            self.changes >= REBUILD_RECOMMENDER_CHANGE_THRESHOLD)
+
     def build_recommender(self):
+        if not self.out_of_date():
+            return self.recommender
+
         user_ics = np.array(self.user_ics, dtype=np.uint32)
         item_ics = np.array(self.item_ics, dtype=np.uint32)
         ratings = np.array(self.ratings, dtype=np.int32)
