af4005719c27da2648f9a00d9cbbf8bcc8dfbb51
Christoph Schock <chschock@gmail.com>
2017-02-20 00:21:32 +0100

    script to insert and query book data

--- tests/book_data.py
+++ tests/book_data.py
@@ -3,11 +3,30 @@ import click
 import csv
 import requests
 import json
+import random
+import time
 from urllib.parse import urljoin
+
 
 BATCH_SIZE = 10000
 
-@click.command()
+def load(likefile):
+    user_item_tuples = []
+    with open(likefile, encoding='latin1') as f:
+        r = csv.reader(f, delimiter=';', skipinitialspace=True, quoting=csv.QUOTE_ALL)
+        next(r) # header row
+        for row in r:
+            if row[2] != '0' and row[2] < '5':
+                continue
+            user_item_tuples.append((row[0], row[1]))
+    return user_item_tuples
+
+
+@click.group()
+def grp():
+    pass
+
+@grp.command()
 @click.argument('likefile', type=click.Path())
 @click.option('--url', default="http://127.0.0.1:5000/", type=click.STRING)
 @click.option('--delete-all-data', default=False, is_flag=True)
@@ -19,14 +38,7 @@ def ingest(likefile, url, delete_all_data):
     if delete_all_data:
         requests.request('DELETE', _url('v1/maintenance/delete-all-data'))
 
-    user_item_tuples = list()
-    with open(likefile, encoding='latin1') as f:
-        r = csv.reader(f, delimiter=';', skipinitialspace=True, quoting=csv.QUOTE_ALL)
-        next(r) # header row
-        for row in r:
-            if row[2] != '0' and row[2] < '5':
-                continue
-            user_item_tuples.append((row[0], row[1]))
+    user_item_tuples = load(likefile)
 
     for i in range(0, len(user_item_tuples), BATCH_SIZE):
         data = {'likes': user_item_tuples[i: i+BATCH_SIZE]}
@@ -35,5 +47,23 @@ def ingest(likefile, url, delete_all_data):
             data=json.dumps(data),
             headers={'content-type':'application/json'})
 
+@grp.command()
+@click.argument('likefile', type=click.Path())
+@click.argument('count', type=click.INT)
+@click.option('--url', default="http://127.0.0.1:5000/", type=click.STRING)
+def recommend(likefile, count, url):
+
+    def _url(endpoint):
+        return urljoin(url, endpoint)
+
+    user_item_tuples = load(likefile)
+
+    start = time.time()
+    for user_id, item_id in random.sample(user_item_tuples, count):
+        res = requests.request('GET',
+            _url('/v1/recommendations/user/{}?count={}'.format(user_id, 6)))
+        print(res.json())
+    print('querying took %4.2f seconds' % (time.time()-start))
+
 if __name__ == '__main__':
-    ingest()
+    grp()
